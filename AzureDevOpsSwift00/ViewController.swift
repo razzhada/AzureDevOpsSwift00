3c038a4b97f8e63108a8f7ad8ff6f19a873e6ba0
Hadi Razzak <hadi_razzak@huntsman.com>
2022-06-23 21:02:48 -0500

    okay

--- AzureDevOpsSwift00/ViewController.swift
+++ AzureDevOpsSwift00/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("view load")
+    print("view loading")
     // Do any additional setup after loading the view.
   }
 
